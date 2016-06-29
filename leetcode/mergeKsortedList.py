c1ded1abcc2232d5e658a91541cf294eeefd4abe
Luozhen <luozhen523cherish@outlook.com>
2016-06-29 17:19:34 +0800

    binary tree from right sight

---
+++ leetcode/mergeKsortedList.py
@@ -0,0 +1,16 @@
+#! user/bin/env python
+# -*- coding:utf-8 -*-
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        pointer = [i for i in lists]
